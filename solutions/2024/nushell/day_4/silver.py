75b1c28d147bc27ff7602cfe567d62d25d2016cf
amtoine <stevan.antoine@gmail.com>
2024-12-05 10:22:41 +0100

    remove prints from day4 gold python

--- solutions/2024/nushell/day_4/silver.py
+++ solutions/2024/nushell/day_4/silver.py
@@ -9,25 +9,21 @@ n, m = len(grid), len(grid[0])
 
 candidates = []
 
-print("horiz")
 for i in range(n):
     for j in range(m - 3):
         candidate = [grid[i][j + dj] for dj in range(4)]
         candidates.append(candidate)
 
-print("vert")
 for i in range(n - 3):
     for j in range(m):
         candidate = [grid[i + di][j] for di in range(4)]
         candidates.append(candidate)
 
-print("diag 1")
 for i in range(n - 3):
     for j in range(m - 3):
         candidate = [grid[i + d][j + d] for d in range(4)]
         candidates.append(candidate)
 
-print("diag 2")
 for i in range(n - 3):
     for j in range(3, m):
         candidate = [grid[i + d][j - d] for d in range(4)]
